0c652e57e29fc970f8017e46cd971fc3520ca824
fhurtadoc <fabiohurtadoc@gmail.com>
2022-03-09 14:46:19 -0500

    plantilla

---
+++ src/config.js
@@ -0,0 +1,10 @@
+let localhost={
+    host:'localhost',
+    user: 'codigodeplaton',
+    password:'12345',
+    database:'autolujos_universo',
+    port:'3306'    
+}
+
+module.exports = localhost;
+
